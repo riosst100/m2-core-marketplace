547e042db12e0f15eb237fc846dd7719ab1be78e
riosusanto <rio.susanto@sirclo.com>
2023-12-22 04:03:27 +0000

    Add category import export module

---
+++ src/module-category-importexport/Block/Adminhtml/CategoryExport/Edit/Tabs.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace CoreMarketplace\CategoryImportExport\Block\Adminhtml\CategoryExport\Edit;
+
+/**
+ * @method Tabs setTitle(\string $title)
+ */
+class Tabs extends \Magento\Backend\Block\Widget\Tabs
+{
+    /**
+     * constructor
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setId('category_tabs');
+        $this->setDestElementId('edit_form');
+        $this->setTitle(__('Export Categories'));
+    }
+}
---
+++ src/module-category-importexport/Block/Adminhtml/CategoryImport/Edit/Tabs.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace CoreMarketplace\CategoryImportExport\Block\Adminhtml\CategoryImport\Edit;
+
+/**
+ * @method Tabs setTitle(\string $title)
+ */
+class Tabs extends \Magento\Backend\Block\Widget\Tabs
+{
+    /**
+     * constructor
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setId('import_tabs');
+        $this->setDestElementId('edit_form');
+        $this->setTitle(__('Import Categories'));
+    }
+}
