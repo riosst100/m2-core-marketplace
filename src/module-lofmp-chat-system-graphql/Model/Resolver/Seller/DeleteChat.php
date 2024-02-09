38498142b260e5304b8a806c2ca5306ce9598724
riosusanto <rio.susanto@sirclo.com>
2024-02-09 04:58:35 +0000

    Fix wrong namespace

--- src/module-lofmp-chat-system-graphql/Model/Resolver/Seller/DeleteChat.php
+++ src/module-lofmp-chat-system-graphql/Model/Resolver/Seller/DeleteChat.php
@@ -27,7 +27,7 @@ use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
-use Lof\MarketPlaceGraphQl\Api\MessageRepositoryInterface;
+use Lof\MarketplaceGraphQl\Api\MessageRepositoryInterface;
 use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
 
 class DeleteChat implements ResolverInterface
